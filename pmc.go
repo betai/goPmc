dd8e8019095d1634b01bd2fd1e1d6f4295b2577e
Betai Koffi <betai@xamarin.com>
2015-09-10 23:02:58 -0700

    add algorithms 2, 3 and 4

--- pmc.go
+++ pmc.go
@@ -6,6 +6,7 @@ package pmc
 import (
 	"fmt"
 	"math/rand"
+	"math"
 	"time"
 	"hash/fnv"
 	"strings"
@@ -15,11 +16,17 @@ import (
 	"github.com/willf/bitset"
 )
 
+var ( // change to const
+	n_max float64 = 1e5
+	phi_const float64 = 0.77351
+)
+
 type Sketch struct {
 	m uint
 	w uint
 	l uint
 	bitmap *bitset.BitSet
+	n uint
 }
 
 func New(l uint, m uint, w uint) (*Sketch, error) {
@@ -31,25 +38,61 @@ func New(l uint, m uint, w uint) (*Sketch, error) {
 	return &Sketch{l: l, m: m, w: w, bitmap: bitset.New(l)}, nil
 }
 
-
 // Algorithm 1 B[H(f,i,j)] = 1
 func (sketch *Sketch) PmcCount(f string) {
-	// i := rand.Intn(m)
-	// j := geometric(w)
-
-	i := 234
-	j := 345
-
-	fij := concatenate(f,i,j)
-	fmt.Printf("fij = %v\n", fij)
-	
-	index := hash(fij)
-	fmt.Printf("index = %v\n", index)
-
-	sketch.bitmap.Set(uint(index) % sketch.l)
+	index := sketch.getIndexF(f)
+	sketch.bitmap.Set(index % sketch.l)
 }
 
 // Algorithm 2
+func (sketch *Sketch) getZSum(f string) (z uint) {
+	z = 0
+	for i := uint(0); i < sketch.m; i++ {
+		for j := uint(0); j < sketch.w; j++ {
+			if !sketch.bitmap.Test(getIndexFIJ(f, i, j)) {
+				z += j - 1
+				break
+			}
+		}
+	}
+	return z
+}
+
+// Algorithm 3
+func (sketch *Sketch) getEmptyRows(f string) (k uint) {
+	k = 0
+	for i := uint(0); i < sketch.m; i ++ {
+		if !sketch.bitmap.Test(getIndexFIJ(f, i, 1)) {
+			k++
+		}
+	}
+	return k
+}
+
+// Algorithm 4
+func (sketch *Sketch) PmcEstimate(f string) (float64, error) {
+	k := float64(sketch.getEmptyRows(f))
+	p := sketch.p()
+	m := float64(sketch.m)
+
+	estimate := 0.0
+	
+	if kp := k/(1 - p); kp > 0.3*m {
+		fmt.Println("small multiplicity")
+		estimate = -2*m*math.Log(kp/m)
+	} else {
+		fmt.Println("large multiplicity")		
+		if sketch.n <= 0 {
+			return -1, errors.New("sketch.n should be positive")
+		}
+		z := float64(sketch.getZSum(f))
+		estimate = m*math.Pow(2, z/m)/sketch.phi(p)
+	}
+
+	estimate = math.Ceil(math.Abs(estimate))
+	sketch.n = uint(estimate)
+	return estimate, nil
+}
 
 
 /**** Public Helpers ****/
@@ -66,6 +109,61 @@ func (sketch *Sketch) PrintBitmap() {
 }
 
 /**** Private Helpers ****/
+func (sketch *Sketch) p() (float64) {
+	count := 0.0
+	for i := uint(0); i < sketch.l ; i++ {
+		if sketch.bitmap.Test(i) {
+			count++
+		}
+	}
+	return count/float64(sketch.l)
+}
+
+func (sketch *Sketch) phi(p float64) (float64) {
+	n := float64(sketch.n)
+	if n >= n_max {
+		return phi_const
+	}
+	return math.Pow(2, sketch.expZ(n, p))/n
+}
+
+func (sketch *Sketch) expZ(n float64, p float64) (float64) {
+	exp := 0.0
+	w := float64(sketch.w)
+	for k := float64(0); k < w; k++ {
+		exp += k * (qk(k, n, p) - qk(k+1, n, p))
+	}
+	return exp
+}
+
+func qk(k float64, n float64, p float64) (float64) {
+	product := 1.0
+	for i := float64(0); i < k; i++ {
+		product *= (1 - math.Pow((1 - math.Pow(2, -i)), n) * (1 - p))
+	}
+	return product
+}
+
+// Get index into sketch bitmap
+func (sketch *Sketch) getIndexF(f string) (uint) { // TODO: Bubble up the error
+	i := uint(rand.Intn(int(sketch.m)))
+	j, err := geometric(sketch.w)
+	if err != nil {
+		fmt.Printf("%v\n", err.Error())
+		return 0  
+	}
+
+	return getIndexFIJ(f, i, j)
+}
+
+func getIndexFIJ(f string, i uint, j uint) (uint) {
+	fij := concatenate(f, int(i), int(j))
+	// fmt.Printf("fij = %v\n", fij)
+	
+	index := hash(fij)
+	// fmt.Printf("index = %v\n", index)
+	return uint(index)
+}
 
 // Join f i and j into a string
 func concatenate(f string, i int, j int) (string) {
@@ -80,7 +178,7 @@ func hash(fij string) (uint64) {
 }
 
 // random number generator based of geometric distribution
-func geometric(w uint) (j int, e error) {
+func geometric(w uint) (j uint, e error) {
 	if w > 32 || w < 0 {
 		return 0, errors.New("input parameter w to geometric function must be > 0")
 	}
@@ -88,8 +186,6 @@ func geometric(w uint) (j int, e error) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	uniform := uint(rand.Int())
 
-	fmt.Printf("uniform = %v \t", uniform)
-	
 	for i := uint(0); i < w; i++ {
 		if uniform & (1 << uint(i)) != 0 {
 			return j, nil
