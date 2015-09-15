3f05e2e28d8fad700c8262f31dffd2e710fea57f
Betai Koffi <betai@xamarin.com>
2015-09-14 17:15:18 -0700

    move to uint64 and fix hashing issue for small multiplicities

--- pmc.go
+++ pmc.go
@@ -7,13 +7,10 @@ import (
 	"fmt"
 	"math/rand"
 	"math"
-	"time"
-	"hash/fnv"
-	"strings"
-	"strconv"
 	"errors"
 
 	"github.com/willf/bitset"
+	"github.com/dgryski/go-farm"
 )
 
 var ( // change to const
@@ -22,39 +19,48 @@ var ( // change to const
 )
 
 type Sketch struct {
-	m uint
-	w uint
-	l uint
+	m uint64
+	w uint64
+	l uint64
 	bitmap *bitset.BitSet
-	n uint
+	n uint64
+	cachedP float64
+	changed bool
 }
 
-func New(l uint, m uint, w uint) (*Sketch, error) {
+func New(l uint64, m uint64, w uint64) (*Sketch, error) {
  	if l == 0 || m == 0 || w == 0 {
 		return nil, errors.New("All parameters must be > 0")
-	} else if l > (1 << w){
-		return nil, errors.New("l must be < 2**w")
+	} else if l > (1 << w) || l > (1 << 32){
+		return nil, errors.New("l must be < 2**w and <= 2**32")
 	}
-	return &Sketch{l: l, m: m, w: w, bitmap: bitset.New(l)}, nil
+	return &Sketch{l: l, m: m, w: w, bitmap: bitset.New(uint(l)), changed: true}, nil
 }
 
 // Algorithm 1 B[H(f,i,j)] = 1
-func (sketch *Sketch) PmcCount(f string) (error) {
+func (sketch *Sketch) PmcCount(f string) error {
 	index, err := sketch.getIndexF(f)
 	if err != nil {
-		//fmt.Printf("%v\n", err.Error())		
+		//fmt.Printf("%v\n", err.Error())
 		return err
 	}
-	sketch.bitmap.Set(index % sketch.l)
+	if sketch.bitmap.Test(uint(index) % uint(sketch.l)) {
+
+		// fmt.Printf("Bit at index %v is already set. Sketch changed %v\n", index, sketch.changed)
+		return nil
+	}
+
+	sketch.changed = true
+	sketch.bitmap.Set(uint(index) % uint(sketch.l))
 	return nil
 }
 
 // Algorithm 2
-func (sketch *Sketch) getZSum(f string) (z uint) {
-	z = 0
-	for i := uint(0); i < sketch.m; i++ {
-		for j := uint(0); j < sketch.w; j++ {
-			if !sketch.bitmap.Test(getIndexFIJ(f, i, j)) {
+func (sketch *Sketch) getZSum(f string) uint64 {
+	z := uint64(0)
+	for i := uint64(0); i < sketch.m; i++ {
+		for j := uint64(0); j < sketch.w; j++ {
+			if !sketch.bitmap.Test(uint(sketch.getIndexFIJ(f, i, j))) {
 				z += j - 1
 				break
 			}
@@ -64,18 +70,22 @@ func (sketch *Sketch) getZSum(f string) (z uint) {
 }
 
 // Algorithm 3
-func (sketch *Sketch) getEmptyRows(f string) (k uint) {
-	k = 0
-	for i := uint(0); i < sketch.m; i ++ {
-		if !sketch.bitmap.Test(getIndexFIJ(f, i, 1)) {
+func (sketch *Sketch) getEmptyRows(f string) uint64 {
+	k := uint64(0)
+	for i := uint64(0); i < sketch.m; i ++ {
+		if !sketch.bitmap.Test(uint(sketch.getIndexFIJ(f, i, 0))) {
 			k++
 		}
+		if sketch.bitmap.Test(uint(sketch.getIndexFIJ(f, i, 0))) {
+			// fmt.Printf ("non empty row")
+		}
 	}
+	// fmt.Printf("\n")
 	return k
 }
 
 // Algorithm 4
-func (sketch *Sketch) PmcEstimate(f string) (float64, error) {
+func (sketch *Sketch) PmcEstimate(f string) (uint64, error) {
 	k := float64(sketch.getEmptyRows(f))
 	p := sketch.p()
 	m := float64(sketch.m)
@@ -83,20 +93,20 @@ func (sketch *Sketch) PmcEstimate(f string) (float64, error) {
 	estimate := 0.0
 	
 	if kp := k/(1 - p); kp > 0.3*m {
-		fmt.Println("small multiplicity")
+		//fmt.Println("Small multiplicity")
 		estimate = -2*m*math.Log(kp/m)
 	} else {
-		fmt.Println("large multiplicity")		
+		//fmt.Println("large multiplicity")		
 		if sketch.n <= 0 {
-			return -1, errors.New("sketch.n should be positive")
+			return 0, errors.New("sketch.n should be positive")
 		}
 		z := float64(sketch.getZSum(f))
 		estimate = m*math.Pow(2, z/m)/sketch.phi(p)
 	}
 
 	estimate = math.Ceil(math.Abs(estimate))
-	sketch.n = uint(estimate)
-	return estimate, nil
+	sketch.n = uint64(estimate)
+	return sketch.n, nil
 }
 
 
@@ -104,7 +114,7 @@ func (sketch *Sketch) PmcEstimate(f string) (float64, error) {
 func (sketch *Sketch) PrintBitmap() {
 	fmt.Printf("Non-zero bits for sketch %v\n", sketch)
 	count := 0
-	for i := uint(0); i < sketch.l ; i++ {
+	for i := uint(0); i < uint(sketch.l) ; i++ {
 		if sketch.bitmap.Test(i) {
 			fmt.Printf("  %v\n", i)
 			count++
@@ -114,17 +124,22 @@ func (sketch *Sketch) PrintBitmap() {
 }
 
 /**** Private Helpers ****/
-func (sketch *Sketch) p() (float64) {
+func (sketch *Sketch) p() float64 {
+	if !sketch.changed {
+		return sketch.cachedP
+	}
 	count := 0.0
-	for i := uint(0); i < sketch.l ; i++ {
+	for i := uint(0); i < uint(sketch.l) ; i++ {
 		if sketch.bitmap.Test(i) {
 			count++
 		}
 	}
-	return count/float64(sketch.l)
+	sketch.cachedP = count/float64(sketch.l)
+	sketch.changed = false
+	return sketch.cachedP
 }
 
-func (sketch *Sketch) phi(p float64) (float64) {
+func (sketch *Sketch) phi(p float64) float64 {
 	n := float64(sketch.n)
 	if n >= n_max {
 		return phi_const
@@ -132,7 +147,7 @@ func (sketch *Sketch) phi(p float64) (float64) {
 	return math.Pow(2, sketch.expZ(n, p))/n
 }
 
-func (sketch *Sketch) expZ(n float64, p float64) (float64) {
+func (sketch *Sketch) expZ(n float64, p float64) float64 {
 	exp := 0.0
 	w := float64(sketch.w)
 	for k := float64(0); k < w; k++ {
@@ -141,7 +156,7 @@ func (sketch *Sketch) expZ(n float64, p float64) (float64) {
 	return exp
 }
 
-func qk(k float64, n float64, p float64) (float64) {
+func qk(k float64, n float64, p float64) float64 {
 	product := 1.0
 	for i := float64(0); i < k; i++ {
 		product *= (1 - math.Pow((1 - math.Pow(2, -i)), n) * (1 - p))
@@ -150,49 +165,31 @@ func qk(k float64, n float64, p float64) (float64) {
 }
 
 // Get index into sketch bitmap
-func (sketch *Sketch) getIndexF(f string) (uint, error) { // TODO: Bubble up the error
-	i := uint(rand.Intn(int(sketch.m)))
+func (sketch *Sketch) getIndexF(f string) (uint64, error) {
+	i := uint64(rand.Int63n(int64(sketch.m))) % sketch.m
 	j, err := geometric(sketch.w)
 	if err != nil {
 //		fmt.Printf("%v\n", err.Error())
 		return 0, err
 	}
 
-	return getIndexFIJ(f, i, j), err
+	return sketch.getIndexFIJ(f, i, j), err
 }
 
-func getIndexFIJ(f string, i uint, j uint) (uint) {
-	fij := concatenate(f, int(i), int(j))
-	// fmt.Printf("fij = %v\n", fij)
-	
-	index := hash(fij)
-	// fmt.Printf("index = %v\n", index)
-	return uint(index)
-}
-
-// Join f i and j into a string
-func concatenate(f string, i int, j int) (string) {
-	return strings.Join([]string{f, strconv.Itoa(i), strconv.Itoa(j)}, "")
-}
-
-// fnv1a hash
-func hash(fij string) (uint64) {
-	h := fnv.New64a()
-	h.Write([]byte(fij))
-	return h.Sum64()
+func (sketch *Sketch) getIndexFIJ(f string, i uint64, j uint64) uint64 {
+	return farm.Hash64WithSeeds([]byte(f), i, j) % sketch.l
 }
 
 // random number generator based of geometric distribution
-func geometric(w uint) (j uint, e error) {
-	if w > 32 {
-		return 0, errors.New("input parameter w to geometric function must be < 32") // uint is 32 bit
+func geometric(w uint64) (j uint64, e error) {
+	if w > 64 {
+		return 0, errors.New("input parameter w to geometric function must be <= 64") // uint64 is 64 bit
 	}
-	
 	uniform := rand.Uint32()
 
 	for i := uint32(0); i < uint32(w); i++ {
 		if uniform & (1 << uint32(i)) != 0 {
-			return uint(i), nil
+			return uint64(i), nil
 		}
 	}
 	return w, nil
