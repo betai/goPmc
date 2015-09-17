e07cb77aa59e6b5a51d7b82784aa07cb773d4061
Betai Koffi <betai@xamarin.com>
2015-09-16 18:10:17 -0700

    add benchmark

---
+++ benchmark/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"math"
+	"log"
+	"os"
+	"strconv"
+	".." //goPmc
+)
+
+func main() {
+	file, _ := os.Open("zipf.csv")
+
+	r := csv.NewReader(file)
+	r.Comma = ';'
+
+	l := uint64(18e6)
+	m := uint64(64)
+	w := uint64(32)
+	
+	fmt.Printf("l = %v\tm = %v\tw= %v\n", l, m, w)
+	pmc, err := pmc.New(l, m, w)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	multiset := NewMultiset()
+	
+	x := 0
+	for {
+		record, err := r.Read()
+		x++
+		if x == 1 {
+			continue
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id := fmt.Sprintf("flow-%s", record[0])
+		counts, _ := strconv.ParseFloat(record[1], 64)
+		
+		for i := 0.0; i < counts; i++ {
+			if x <= 51 { // just take the first 50
+				multiset.Add(id)
+			}
+			pmc.PmcCount([]byte(id))
+		}
+	}
+
+	keys := multiset.Keys()
+	min := math.MaxFloat64
+	max := 0.0
+	avg := 0.0
+	std := 0.0
+	
+	for _, key := range keys {
+		est, err := pmc.PmcEstimate([]byte(key))
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		actual := float64(multiset.Get(key))
+		diff := 100*math.Abs(float64(est) - actual)/actual
+		
+		if diff > max {
+			max = diff
+		}
+		if diff < min {
+			min = diff
+		}
+
+		avg += diff
+		std += diff*diff
+		
+		fmt.Printf("id: %v\texpected: %v\test: %v\t(1 - est/expected[i])*100: %v%%\n", key, actual, est, diff)
+	}
+
+	n := float64(len(keys))
+	fmt.Printf("error: min/max/avg/std  %v / %v / %v / %v\n", min, max, avg/n, math.Sqrt(std/(n - 1.0)))
+	fmt.Println("fill rate:", pmc.GetFillRate())
+}
---
+++ benchmark/multiset.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Multiset struct {
+	set map[string]int
+}
+
+func NewMultiset () *Multiset {
+	return &Multiset{set: make(map[string]int)}
+}
+
+func (multiset *Multiset) Len() int {
+	return len(multiset.set)
+}
+
+func (multiset *Multiset) Add(key string) {
+	_, ok := multiset.set[key]
+	if !ok {
+		multiset.set[key] = 0
+	}
+	multiset.set[key]++
+}
+
+func (multiset *Multiset) Get(key string) (int, bool) {
+	return multiset.set[key]
+}
+
+
+func (multiset *Multiset) Keys() []string {
+	keys := make([]string, 0, len(multiset.set))
+	for key, _ := range multiset.set {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (multiset *Multiset) PrintKeys() {
+	for key, value := range multiset.set {
+		fmt.Printf("m[%v] = %v\n", key, value)
+	}
+}
+
+func (multiset *Multiset) PrintCount() {
+	sum := 0
+	for _, value := range multiset.set {
+		sum += value
+	}
+	fmt.Printf("the sum is %v\n", sum)
+}
